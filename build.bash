0b2a25e7b59285d59912b4ecbc975dfd285703b8
pverney <pverney@4a32156a-8e32-406b-b38d-595c535062e7>
2013-12-06 14:12:25 +0000

    fix build
    git-svn-id: http://svn.ives.fr/svn-libs-dev/asterisk/fontventa/trunk@5597 4a32156a-8e32-406b-b38d-595c535062e7

--- build.bash
+++ build.bash
@@ -2,7 +2,7 @@
 rm -f *.rpm
 chmod 755 install.ksh
 sudo yum -y remove "ffmpeg*" x264
-sudo yum -y install ffmpeg-0.4.1 ffmpeg-devel-0.4.1 asteriskv-devel mpeg4ip-devel
+sudo yum -y install ffmpeg-0.4.2 ffmpeg-devel-0.4.2 asteriskv-devel mpeg4ip-devel amrnb gsm lame
 sudo yum -y install asteriskv
 sudo yum -y install SDL-devel
 ./install.ksh rpm nosign
