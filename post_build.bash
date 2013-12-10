343aa491bc5b0004ffd7f5929384b8fba908659e
pverney <pverney@4a32156a-8e32-406b-b38d-595c535062e7>
2013-12-10 08:43:57 +0000

    fix test
    git-svn-id: http://svn.ives.fr/svn-libs-dev/asterisk/fontventa/trunk@5629 4a32156a-8e32-406b-b38d-595c535062e7

--- post_build.bash
+++ post_build.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum -y remove fontventa
 sudo yum localinstall --nogpgcheck -y fontventa*.rpm
-/usr/bin/IVES_convert.ksh -i PUB_Guerlain_-_La_petite_robe_noire.mp4 -o test_convert.mp4
+IVES_convert.ksh -i PUB_Guerlain_-_La_petite_robe_noire.mp4 -o test_convert.mp4
 ENVOYER_VISIO2=$1
 if [ "$ENVOYER_VISIO2" != "" ] 
 then
