ca93e1e1946177f31b29e4b93adeebb3f88fa265
omaquin <omaquin@4a32156a-8e32-406b-b38d-595c535062e7>
2013-12-11 10:34:42 +0000

    commentaire envoie visio2
    git-svn-id: http://svn.ives.fr/svn-libs-dev/asterisk/fontventa/trunk@5649 4a32156a-8e32-406b-b38d-595c535062e7

--- post_build.bash
+++ post_build.bash
@@ -5,6 +5,7 @@ IVES_convert.ksh -i PUB_Guerlain_-_La_petite_robe_noire.mp4 -o test_convert.mp4
 ENVOYER_VISIO2=$1
 if [ "$ENVOYER_VISIO2" != "" ] 
 then
+    echo "On envoie sur visio2"
     scp -oPort=2222 *.rpm jenkins@visio2.visioassistance.net:/home/jenkins/$NODE_NAME
 else
     echo "On n'envoie rien sur visio2"
