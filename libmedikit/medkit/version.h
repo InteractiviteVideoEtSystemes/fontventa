e9becddf46f2ac9a374af5d11a3a497c6867ea27
ebuu <ebuu@4a32156a-8e32-406b-b38d-595c535062e7>
2014-02-24 10:54:29 +0000

    Fichier manquant
    git-svn-id: http://svn.ives.fr/svn-libs-dev/asterisk/fontventa/trunk@5984 4a32156a-8e32-406b-b38d-595c535062e7

---
+++ libmedikit/medkit/version.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   version.h
+ * Author: Sergio
+ *
+ * Created on 3 de diciembre de 2012, 18:11
+ */
+
+#ifndef VERSION_H
+#define	VERSION_H
+
+#define MCUVERSION	"1.5.1"
+#define MCUDATE		"$Date: 2013-06-24 15:02:42 +0200 (lun., 24 juin 2013) $"
+
+#endif	/* VERSION_H */
+
