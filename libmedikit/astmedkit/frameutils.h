84906c6c31526a688e178d7336deb577bbda2575
ebuu <ebuu@4a32156a-8e32-406b-b38d-595c535062e7>
2015-05-07 08:10:55 +0000

    utilsiation framutils en C
    git-svn-id: http://svn.ives.fr/svn-libs-dev/asterisk/fontventa/trunk@7170 4a32156a-8e32-406b-b38d-595c535062e7

--- libmedikit/astmedkit/frameutils.h
+++ libmedikit/astmedkit/frameutils.h
@@ -8,6 +8,8 @@
 #define PKT_SIZE        (sizeof(struct ast_frame) + AST_FRIENDLY_OFFSET + PKT_PAYLOAD)
 #define PKT_OFFSET      (sizeof(struct ast_frame) + AST_FRIENDLY_OFFSET)
 
+#ifdef __cplusplus
+
 int AstFormatToCodecList(int format, AudioCodec::Type codecList[], unsigned int maxSize);
 int AstFormatToCodecList(int format, VideoCodec::Type codecList[], unsigned int maxSize);
 
@@ -26,3 +28,4 @@ bool MediaFrameToAstFrame(const MediaFrame * mf, ast_frame & astf);
 int CodecToAstFormat( AudioCodec::Type ac, int & fmt );
 int CodecToAstFormat( VideoCodec::Type vc, int & fmt );
 
+#endif
